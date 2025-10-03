64b8ff9d903756113be37192b070c7de11e3ef4f
Jitendra Kumar <JK@MacBook-Pro.local>
2025-10-03 21:30:25 +0530

    Sum of series

---
+++ sumOfSerie.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int sumOfSeries(int,int);
+
+int main(){
+	int num,power;
+	printf("Enter number : ");
+	scanf("%d",&num);
+	printf("\nEnter power : ");
+	scanf("%d",&power);
+	printf("\nSum of Series : %d\n",sumOfSeries(num,power));
+	return 0;
+}
+
+int sumOfSeries(int num,int power){
+	if(power == 0) return 1;
+	return pow(num,power) + sumOfSeries(num,power-1);
+}
