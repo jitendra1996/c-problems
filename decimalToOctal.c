476735f168708c94938a9a76cbbb273fb1df000f
Jitendra Kumar <JK@MacBook-Pro.local>
2025-10-04 21:03:49 +0530

    Fixed octal bug

--- decimalToOctal.c
+++ decimalToOctal.c
@@ -4,29 +4,9 @@ int octal(int);
 int reverseNum(int);
 
 int main(){
-	int i , oct;
+	int i ;
 	printf("Enter a decimal number : ");
 	scanf("%d",&i);
-	oct = octal(i);
-	printf("\noctal number : %d\n",oct);
+	printf("\noctal number : %o\n",i);
 	return 0;
-}
-
-int octal(int num){
-	int sum=0 ,rem;
-	while(num != 0){
-		rem = num % 8 ;
-		sum = sum*10 + rem;
-		num /= 8 ; 
-	}
-	return reverseNum(sum); 
-}
-
-int reverseNum(int num){
-	int temp = 0;
-	while(num != 0){
-		temp = temp * 10 + (num % 10);
-		num /= 10;
-	}
-	return temp;
 }
